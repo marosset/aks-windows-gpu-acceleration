4d1815f8aa8645a6a3af9da4bf6830851923dbfe
Daniel Heinze <daheinze@microsoft.com>
2023-02-14 11:36:22 +0100

    feat: updated daemonset and scripts

--- test_scenarios/scripts/configure_vmss.sh
+++ test_scenarios/scripts/configure_vmss.sh
@@ -1,60 +1,33 @@
 #!/bin/bash
-#
-# Configure GPU acclerated Windows nodes in AKS cluster
-# Following this example: https://github.com/marosset/aks-windows-gpu-acceleration
 
 set -euo pipefail
 
 # Global vars
-readonly WIN_NODEPOOL_NAME="npwin"
-readonly WIN_RESOURCE_GROUP="MC_akswintest_testaksclusterwithwingpu_northeurope"
+readonly WIN_NAMESPACE="windows-nodepool"
 
 #######################################
-# Add the 'NVDIA GPU Driver Extension' to the VirtualMachineScaleSet created for the new node pool added before
+# Apply daemonset to configure GPU nodes
 # Globals:
-#   RESOURCE_GROUP_NAME
-#   AKS_CLUSTER_NAME
-#   WIN_NODEPOOL_NAME
+#   WIN_NAMESPACE
 # Arguments:
 #   None
 # Outputs:
 #   Writes info to stdout
 #######################################
-add_nvidia_driver_extension() {
-    echo "- Adding extension"
-    az vmss extension set \
-    --resource-group $WIN_RESOURCE_GROUP \
-    --vmss-name "aks$WIN_NODEPOOL_NAME" \
-    --name NvidiaGpuDriverWindows \
-    --publisher Microsoft.HpcCompute \
-    --version 1.6 \
-    --settings '{}'
-}
+apply_daemonset() {
 
-#######################################
-# Update the VMSS instances to use the latest model
-# Globals:
-#   WIN_RESOURCE_GROUP
-#   WIN_NODEPOOL_NAME
-# Arguments:
-#   None
-# Outputs:
-#   Writes info to stdout
-#######################################
-update_instances() {
-    az vmss update-instances \
-    --resource-group "$WIN_RESOURCE_GROUP" \
-    --name "aks$WIN_NODEPOOL_NAME" \
-    --instance-ids "*"
+    # create kubernetes namespace
+    kubectl create namespace $WIN_NAMESPACE
+
+    # apply daemonset
+    kubectl apply -f manifests/daemonset-configure-node.yml
 }
 
 run_main() {
-    
-    echo "--- Adding Nvidia driver extension ---"
-    add_nvidia_driver_extension
-    
-    echo "--- Updating Instances ---"
-    update_instances
+
+    echo "--- Applying daemonset to configure GPU nodes ---"
+    apply_daemonset
+
 }
 
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
--- test_scenarios/scripts/create_infrastructure.sh
+++ test_scenarios/scripts/create_infrastructure.sh
@@ -6,14 +6,15 @@
 set -euo pipefail
 
 # Global vars
-readonly RESOURCE_GROUP_NAME="${1-akswintest}"
-readonly LOCATION="${2-northeurope}"
+readonly RESOURCE_GROUP_NAME="${1-testakswin}"
+readonly LOCATION="${2-westeurope}"
 readonly AKS_NODE_COUNT=2
 readonly WIN_NODE_COUNT=1
-readonly WINDOWS_USERNAME="testuser"
+readonly WINDOWS_USERNAME="azureuser"
 readonly WINDOWS_PASSWORD="testpasswordw1thSpeci@lchars"
 readonly AKS_CLUSTER_NAME="testaksclusterwithwingpu"
 readonly WIN_NODEPOOL_NAME="npwin"
+readonly NODEPOOL_OS_SKU="Windows2022"
 readonly VM_SIZE="Standard_NV8as_v4"
 
 #######################################
@@ -31,46 +32,48 @@ readonly VM_SIZE="Standard_NV8as_v4"
 #   Writes info to stdout
 #######################################
 create_infrastructure() {
-    
+
     echo "- Create resource group $RESOURCE_GROUP_NAME"
     az group create \
-    --location $LOCATION \
-    --name "$RESOURCE_GROUP_NAME"
-    
+        --location $LOCATION \
+        --name "$RESOURCE_GROUP_NAME"
+
     echo ""
     echo "- Create AKS cluster $AKS_CLUSTER_NAME"
-    
+
     az aks create \
-    --resource-group "$RESOURCE_GROUP_NAME" \
-    --name $AKS_CLUSTER_NAME \
-    --node-count $AKS_NODE_COUNT \
-    --enable-addons monitoring \
-    --generate-ssh-keys \
-    --windows-admin-username $WINDOWS_USERNAME \
-    --windows-admin-password $WINDOWS_PASSWORD \
-    --vm-set-type VirtualMachineScaleSets \
-    --network-plugin azure
-    
+        --resource-group "$RESOURCE_GROUP_NAME" \
+        --name $AKS_CLUSTER_NAME \
+        --location $LOCATION \
+        --node-count $AKS_NODE_COUNT \
+        --enable-addons monitoring \
+        --generate-ssh-keys \
+        --windows-admin-username $WINDOWS_USERNAME \
+        --windows-admin-password $WINDOWS_PASSWORD \
+        --vm-set-type VirtualMachineScaleSets \
+        --network-plugin azure
+
     echo ""
     echo "- Add Windows nodepool $WIN_NODEPOOL_NAME of size $VM_SIZE"
-    
+
     az aks nodepool add \
-    --resource-group "$RESOURCE_GROUP_NAME" \
-    --cluster-name $AKS_CLUSTER_NAME \
-    --os-type Windows \
-    --name $WIN_NODEPOOL_NAME \
-    --node-count $WIN_NODE_COUNT \
-    --node-vm-size $VM_SIZE
-    
+        --resource-group "$RESOURCE_GROUP_NAME" \
+        --name $WIN_NODEPOOL_NAME \
+        --cluster-name $AKS_CLUSTER_NAME \
+        --node-vm-size $VM_SIZE \
+        --os-type Windows \
+        --os-sku $NODEPOOL_OS_SKU \
+        --node-count $WIN_NODE_COUNT
+
     echo ""
     echo "- Conntect to AKS cluster"
     az aks get-credentials \
-    --resource-group "$RESOURCE_GROUP_NAME" \
-    --name $AKS_CLUSTER_NAME
+        --resource-group "$RESOURCE_GROUP_NAME" \
+        --name $AKS_CLUSTER_NAME
 }
 
 run_main() {
-    
+
     echo "--- Creating infrastructure ---"
     create_infrastructure
 }
